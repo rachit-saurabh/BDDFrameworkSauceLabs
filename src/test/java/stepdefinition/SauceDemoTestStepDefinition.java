e65a114a080f1d05c159a5ba2b03ca1694630101
rachitsaurabh <rachit.saurabh123@gmail.com>
2020-05-02 10:10:14 +0530

    added data driven concept

--- src/test/java/stepdefinition/SauceDemoTestStepDefinition.java
+++ src/test/java/stepdefinition/SauceDemoTestStepDefinition.java
@@ -10,12 +10,14 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class SauceDemoTestStepDefinition {
+public class SauceDemoTestStepDefinition 
+{
 
 	WebDriver driver;
 
 	@Given("^user is on the login page$")
-	public void user_is_on_the_login_page() {
+	public void user_is_on_the_login_page() 
+	{
 		System.setProperty("webdriver.chrome.driver", "D:\\library\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -23,33 +25,37 @@ public class SauceDemoTestStepDefinition {
 		driver.get("https://www.saucedemo.com/");
 	}
 
-	@When("^when page title is Swag Labs$")
-	public void when_page_tile_is_Swag_Labs() {
+	@When("^when page title is SwagLabs$")
+	public void when_page_tile_is_SwagLabs() 
+	{
 		String title = driver.getTitle();
-		System.out.println(title);
 		Assert.assertEquals(title, "Swag Labs");
 	}
 
-	@Then("^user enters the username and password$")
-	public void user_enters_the_username_and_password() {
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+	@Then("^user enters the \"(.*)\" and \"(.*)\"$")
+	public void user_enters_the_username_and_password(String username, String password) 
+	{
+		driver.findElement(By.id("user-name")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
 	}
 
 	@Then("^user clicks on login button$")
-	public void user_clicks_on_login_button() {
+	public void user_clicks_on_login_button() 
+	{
 		driver.findElement(By.xpath("//input[@type = 'submit']")).click();
 	}
 
 	@Then("^user redirected to the home page$")
-	public void user_redirected_to_the_home_page() {
-		boolean b = driver.findElement(By.xpath("//*[@id=\"menu_button_container\"]/div/div[3]/div/button"))
-				.isDisplayed();
-		System.out.println(b);
+	public void user_redirected_to_the_home_page()
+	{
+		String title = driver.getTitle();
+		System.out.println(title);
+		Assert.assertEquals("Swag Labs", title);
 	}
 
 	@And("^close the driver$")
-	public void close_the_driver() {
+	public void close_the_driver() 
+	{
 		driver.quit();
 	}
 
--- target/test-classes/features/webtesting.feature
+++ target/test-classes/features/webtesting.feature
@@ -1,11 +1,18 @@
 Feature: SauceDemo Web-site Features Testing
 
-Scenario: Login Page
+Scenario Outline: Login Page
 
 Given user is on the login page
-When when page title is Swag Labs
-Then user enters the username and password
+When when page title is SwagLabs
+Then user enters the "<username>" and "<password>"
 Then user clicks on login button
 Then user redirected to the home page
 And close the driver
+
+Examples:
+         |        username         |     password     |
+         |      standard_user      |   secret_sauce   |
+         |     locked_out_user     |   secret_sauce   |
+         | performance_glitch_user |   secret_sauce   |
+
 
